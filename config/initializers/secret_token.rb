ca0a8e84cfae9062c3927169a6e1cabf9a87753c
Michael Peterson <Michael.Peterson@truestonefed.com>
2012-12-26 20:07:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RandomLife::Application.config.secret_token = '10b686382c39a1199ac7625ecac0789bda83e58b1d8ac0685cea69546c0964083240cdaef75b6d578e07cd26981faef34811e14bf3cb4893b0258b81d6034b88'
